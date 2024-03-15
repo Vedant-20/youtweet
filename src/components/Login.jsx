b41e3413ebd3fac5304348e0745872ab7b0bba21
Vedant <vedantwarjurkar@gmail.com>
2024-03-15 13:23:18 +0530

    frontend errors

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -38,8 +38,8 @@ function Login() {
       const {accessToken,refreshToken}=response.data.data
       // console.log('AccessToken',accessToken)
       // console.log('RefreshToken',refreshToken)
-      Cookies.set('accessToken', accessToken, { path: '/' ,sameSite:'none', secure:true}, );
-      Cookies.set('refreshToken', refreshToken, { path: '/' , sameSite:'none', secure:true });
+      Cookies.set('accessToken', accessToken, { path: '/' } );
+      Cookies.set('refreshToken', refreshToken, { path: '/'  });
       if(response.status===200){
         enqueueSnackbar(response.data.message,{
           variant:'success',
