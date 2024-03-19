ce64cc99ae49d92e1166083ea755ef47dbe9581b
Vedant <vedantwarjurkar@gmail.com>
2024-03-19 11:15:50 +0530

    added new backend link

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:8000',
+      '/api':'https://youtube-backend-g0ub.onrender.com',
       
     },
   },
