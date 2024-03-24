719f117b8bda0417b3c5851cb871c1aac8ed9f26
Vedant <vedantwarjurkar@gmail.com>
2024-03-24 12:49:42 +0530

    trying to disable cookies from frontend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -41,8 +41,8 @@ function Login() {
       const {accessToken,refreshToken}=response.data.data
       // console.log('AccessToken',accessToken)
       // console.log('RefreshToken',refreshToken)
-      Cookies.set('accessToken', accessToken, { path: '/' , secure:true, sameSite:'none', expires:7} );
-      Cookies.set('refreshToken', refreshToken, { path: '/' , secure:true , sameSite:'none' , expires:7,  });
+      // Cookies.set('accessToken', accessToken, { path: '/' , secure:true, sameSite:'none', expires:7} );
+      // Cookies.set('refreshToken', refreshToken, { path: '/' , secure:true , sameSite:'none' , expires:7,  });
       setLoading(false)
       if(response.status===200){
         enqueueSnackbar(response.data.message,{
